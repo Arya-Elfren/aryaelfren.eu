47258dda76683f939efe08e131c374a0d3f40296
Oliver Turner <wvdg57@durham.ac.uk>
2022-10-15 17:24:30 +0100

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,15 @@
   description = "First aryaelfren html output";
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.site = (
-      let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        pkgs.stdenv.mkDerivation {
-          name = "aryaelfren.eu";
-          src = ./.;
-          buildInputs = with pkgs; [];
-          phases = ["buildPhase"];
-            buildPhase = ''
-              mkdir -p $out
-              echo "test" > $out/index.html
-            '';
-          }
-        );
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.site;
+    defaultPackage."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".stdenv.mkDerivation {
+      name = "aryaelfren.eu";
+      src = ./.;
+      # nativeBuildInputs = with nixpkgs; [];
+      phases = ["buildPhase"];
+      buildPhase = ''
+        mkdir -p $out
+        echo "test" > $out/index.html
+      '';
+    };
   };
 }
